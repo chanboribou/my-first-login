086e9e28415cecc73efc4250a370b5b676e548c5
boribou <prakchanboribou@gmail.com>
2023-11-18 14:54:14 +0700

    edits

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
 function validateLogin() {
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
-    const errorMessage = document.getElementById("error-message").value;
+    const errorMessage = document.getElementById("error-message");
+
+    console.log(username)
+
+    if (username === "boribou.prak" && password === "2007") {
+        console.error('here')
+        errorMessage.style.color = "green";
+        errorMessage.textContent = "Login Successful!";
+    } else {
+        errorMessage.style.color = "red";
+        errorMessage.textContent = "Invalid username or password. Please try again.";
+        }
 }
 
-if (username == "prak.chanboribou" && password == "boribou@18072007") {
-    errorMessage.style.color = "green";
-    errorMessage.textContent = "Login Successful!";
-} else {
-    errorMessage.style.color = "red";
-    errorMessage.textContent = "Invalid username or password. Please try again.";
-}
