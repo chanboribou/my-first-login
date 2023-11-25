31db7d71fc36b393c089686f99211337f60823a3
boribou <prakchanboribou@gmail.com>
2023-11-25 14:51:52 +0700

    htmlcssandjs

--- JS/main.js
+++ JS/main.js
@@ -8,7 +8,11 @@ function validateLogin() {
     if (username === "boribou.prak" && password === "2007") {
         console.error('here')
         errorMessage.style.color = "green";
-        errorMessage.textContent = "Login Successful!";
+        errorMessage.textContent = "Login Successful!"; 
+
+        setTimeout(function () {
+            window.location.href = "html/aboutme.html";
+          }, 2000);
     } else {
         errorMessage.style.color = "red";
         errorMessage.textContent = "Invalid username or password. Please try again.";
